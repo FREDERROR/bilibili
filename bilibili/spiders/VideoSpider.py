8b971bd55a7c203ebff3880e84f72cd7b97af133
FREDERROR <935525436@qq.com>
2020-04-27 18:08:51 +0800

    second

--- bilibili/spiders/VideoSpider.py
+++ bilibili/spiders/VideoSpider.py
@@ -7,10 +7,14 @@ from scrapy.http import Request,Response
 
 
 class VideoSpider(scrapy.Spider):
+    # 爬虫名字，用户区分不同的爬虫
     name = 'video'
+    # 定义允许爬取的域名
     allowed_domains = ['bilibili.com']
-    start_urls = ['http://www.bilibili.cexiom/']
-    spider_av = range(settings.get("VIDEO_START"),settings.get("VIDEO_END"))
+    # 定义初试请求
+    start_urls = ['http://www.bilibili.com/']
+    # 爬取的视频编号
+    spider_av = range(settings.get("VIDEO_START"), settings.get("VIDEO_END"))
 
     def start_requests(self):
         for av in self.spider_av:
@@ -22,10 +26,12 @@ class VideoSpider(scrapy.Spider):
             }
             print(f"爬取 av{av}")
             video_request = scrapy.Request(url=f"https://www.bilibili.com/video/av{av}",
-                                           headers = video_header,
+                                           headers=video_header,
                                            callback=self.parse_video_page
                                            )
             yield video_request
+
+    # 负责解析返回的数据
     def parse(self, response):
         if json.loads(response.body)['code'] == 0:
             data = json.loads(response.body)['data']
@@ -39,6 +45,7 @@ class VideoSpider(scrapy.Spider):
             item['like'] = data['like']
             item['now_rank'] = data['now_rank']
             item['his_rank'] = data['his_rank']
+            # 爬取播放量大于100000的视频信息
             if item['view'] > 100000:
                 return item
 
@@ -46,8 +53,8 @@ class VideoSpider(scrapy.Spider):
         item = VideoItem()
         item['up_name'] = response.css('.name a::text').extract_first()
         item['title'] = response.css('.tit::text').extract_first()
-        aid = response.url.replace("https://www.bilibili.com/video/av","")
-        item['aid'] = aid.replace("/","")
+        aid = response.url.replace("https://www.bilibili.com/video/av", "")
+        item['aid'] = aid.replace("/", "")
         timestamp = int(round(time.time()*1000))
         header = {
             "Host":"api.bilibili.com",
@@ -61,7 +68,8 @@ class VideoSpider(scrapy.Spider):
         yield Request(
             url=f"http://api.bilibili.com/archive_stat/stat?callback=&aid={item['aid']}&type=json&_={timestamp}",
             dont_filter=False,
-            headers = header,
+            headers=header,
             callback=self.parse,
-            meta = {"item": item}
+            # item对象传递给下个函数
+            meta={"item": item}
         )
